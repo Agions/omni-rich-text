f4e6369367c41dcfa74aa7819a4d921702bd8363
Agions <1051736049@qq.com>
2026-09-24 10:55:35 +0800

    fix: add ambient react-native types for peer dependency CI build

---
+++ src/types/react-native.d.ts
@@ -0,0 +1,14 @@
+declare module 'react-native' {
+  export const View: any;
+  export const Text: any;
+  export const Image: any;
+  export const Animated: any;
+  export const Pressable: any;
+  export const Linking: any;
+  export const Alert: any;
+  export const ScrollView: any;
+  export const StyleSheet: any;
+  export type ViewStyle = any;
+  export type TextStyle = any;
+  export type ImageStyle = any;
+}
